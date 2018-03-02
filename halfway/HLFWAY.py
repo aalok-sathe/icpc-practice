59c2ce9e98dd9968f3f724cdca10c3586fd70ac7
aalok.sathe <aalok.sathe@gmail.com>
2018-03-02 11:01:48 -0500

    fix undefined variable error using lambda func

--- halfway/HLFWAY.py
+++ halfway/HLFWAY.py
@@ -4,4 +4,4 @@
 import sys
 
 for line in sys.stdin:
-	print (int(.25*(-(2-4*n)-((2-4*n)**2-8*n*(n-1))**.5))+1)
+	print ((lambda n: (int(.25*(-(2-4*n)-((2-4*n)**2-8*n*(n-1))**.5))+1))(int(line.split()[0])))
