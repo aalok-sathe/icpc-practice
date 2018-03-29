80b371b36492e6f689ae8d00f5d36750179b4d39
aalok.sathe <aalok.sathe@gmail.com>
2018-03-29 18:08:13 -0400

    start writing kinfolk.cpp

---
+++ !kinfolk/KINFLK.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<math.h>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int A,B = 0;
+    char genderB;
+    
+    while(A>=0 and B>=0 and (cin >> A >> B >> genderB))
+    {
+        int depthA  = (A>0)? (int) (log(A+1)/log(2)) : 0;
+        int depthB  = (B>0)? (int) (log(B+1)/log(2)) : 0;
+        int widthAB = 
+        
+        cout << depthA << " " << depthB << endl; // DEBUG
+        
+        string relation = string();
+        
+        if(abs(depthA - depthB) > 5)
+            relation += "kin";
+        else
+        
+        cout << relation << endl;
+    }
+    
+	return 0;
+}
