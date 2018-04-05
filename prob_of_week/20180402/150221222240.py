cfe0c7c7c11d36c36d4c83d13680df7f1ad52490
aalok.sathe <aalok.sathe@gmail.com>
2018-04-05 19:05:55 -0400

    modified:   makeChange.py

--- prob_of_week/20180402/150221222240.py
+++ prob_of_week/20180402/150221222240.py
@@ -2,6 +2,42 @@
 
 import modarithmetic
 
+target = int(input("Target?\t"))
+
+denominations = []
+#denominations.reverse()
+
+num_ways = {0:{}, -1:}
+for num in range(target+1):
+    if modarithmetic.isPrime(num,100):
+        denominations.append(num)
+
+
+
+for i in range(1,1+target):
+    num_ways[i] = {1:1,5:0,25:0,50:0,100:0,500:0,2000:0}
+    for d in range(0,len(denominations)):
+        if denominations[d] > i:
+            pass#continue
+        num_ways[i][denominations[d]] = num_ways.get(i-denominations[d],{}).get(denominations[d],0) + num_ways[i][denominations[d-1]]
+        #num_ways[i]["T"] += num_ways[i][denominations[d]]
+
+#for i in range(1,len(num_ways)):
+print(num_ways[target][2000])
+    
+    
+#
+#
+#   change[i,j] =  change[i,j-1]
+#                + change[i-j,j]
+#
+
+
+'''
+#! /bin/env/ python3
+
+import modarithmetic
+
 import progress_printer
 pp = progress_printer.Progress_printer()
 
@@ -45,4 +81,4 @@ for num in numbers:
         print(list(zip(nums,numWays)))
         print(num)
         break
-        
+''' 
